1a0e5a79cade4fe983ab8102b72805778de2af22
Beyza <beyzauyanik57@hotmail.com>
2022-12-01 15:03:46 +0300

    detail sayfası oluşturulmaya başlandı

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'page/detail_page.dart';
 import 'page/home_page.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const DetailPage(),
     );
   }
 }
---
+++ lib/page/detail_page.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({super.key});
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff171721),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Image.asset(
+          "assets/back_icon.png",
+          color: Colors.white,
+        ),
+        actions: [
+          Image.asset(
+            "assets/bookmark_icon.png",
+            color: Colors.white,
+          )
+        ],
+      ),
+      body: Column(children: [
+        Stack(
+          children: [
+            Container(
+              //height: 350,
+              width: double.infinity,
+              child: Image.asset(
+                "assets/top_images.png",
+                fit: BoxFit.cover,
+              ),
+            ),
+            const Positioned(
+              bottom: 0,
+              left: 70,
+              child: Text(
+                "Wonder Women",
+                style: TextStyle(
+                    fontSize: 36,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "2020",
+              style: TextStyle(
+                  color: Colors.white38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: Image.asset("assets/circle.png"),
+            ),
+            const Text(
+              "Adventure, Action",
+              style: TextStyle(
+                  color: Colors.white38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: Image.asset("assets/circle.png"),
+            ),
+            const Text(
+              "2 h 35 min",
+              style: TextStyle(
+                  color: Colors.white38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 17),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "7,2",
+                style: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xffFDC432),
+                    fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                width: 7,
+              ),
+              RatingBar.builder(
+                ignoreGestures: true,
+                unratedColor: Colors.grey,
+                itemSize: 20,
+                initialRating: 2,
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                itemBuilder: (context, _) => Icon(
+                  Icons.star,
+                  color: Color(0xffFDC432),
+                ),
+                onRatingUpdate: (rating) {
+                  print("print yazdırıldı");
+                },
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 15),
+          child: Text(
+            "Wonder Woman squares off against Maxwell Lord and the Cheetah, a villainess who possesses superhuman strength and agility.",
+            style: TextStyle(color: Colors.white38),
+          ),
+        )
+      ]),
+    );
+  }
+}
