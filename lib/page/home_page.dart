7e46fd30e7f6f8b007ed30a5a8ba2aa7b5b2fe41
Beyza <beyzauyanik57@hotmail.com>
2022-12-01 11:53:57 +0300

    home page sayfası tamamlandı

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -15,79 +15,192 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: const Color(0xff171721),
       body: Padding(
-        padding: const EdgeInsets.only(top: 45, left: 15, right: 18.24),
-        child: Column(
-          children: [
-            Row(
-              //appbarr
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: const [
-                    Text(
-                      "Mov",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 36,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    Text(
-                      "ve",
-                      style: TextStyle(
-                          color: Color(0xffE11A38),
-                          fontSize: 36,
-                          fontWeight: FontWeight.w500),
-                    ),
-                  ],
+        padding: const EdgeInsets.only(
+          top: 45,
+          left: 15,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                //appbarr
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: const [
+                      Text(
+                        "Mov",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 36,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        "ve",
+                        style: TextStyle(
+                            color: Color(0xffE11A38),
+                            fontSize: 36,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 18),
+                    child: Image.asset("assets/search_icon.png")
+                  )
+                ],
+              ), //appbar-finish
+              Padding(
+                padding: const EdgeInsets.only(top: 18, bottom: 16),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: const Text(
+                    "Popular Movie",
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Color(0xffFFFFFF),
+                        fontWeight: FontWeight.w500),
+                  ),
                 ),
-                const Icon(
-                  Icons.search,
-                  color: Color(0xffFFFFFF),
-                  size: 28,
-                )
-              ],
-            ), //appbar-finish
-            Padding(
-              padding: const EdgeInsets.only(top: 18, bottom: 16),
-              child: Container(
+              ),
+              Container(
+                // color: Colors.blue,
+                /// width: MediaQuery.of(context).size.width,
+                height: 240,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset("assets/popular_image.png"),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 6, bottom: 3),
+                            child: Text(
+                              "Wonder Women (1984)",
+                              style: TextStyle(
+                                  color: Color(0xffFFFFFF),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          const Text(
+                            "Des 16, 2020",
+                            style:
+                                TextStyle(color: Colors.white38, fontSize: 12),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 16),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: const Text(
+                    "TV Show",
+                    style: TextStyle(
+                        color: Color(0xffFFFFFF), fontWeight: FontWeight.w500,fontSize: 18),
+                  ),
+                ),
+              ),
+              Container(
+                //color: Colors.blue,
                 width: MediaQuery.of(context).size.width,
-                child: const Text(
-                  "Popular Movie",
-                  style: TextStyle(
-                      fontSize: 18,
-                      color: Color(0xffFFFFFF),
-                      fontWeight: FontWeight.w500),
+                height: 220,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset("assets/tv_image.png"),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 6, bottom: 3),
+                            child: Text(
+                              "30 Monedas",
+                              style: TextStyle(
+                                  color: Color(0xffFFFFFF),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          const Text(
+                            "Des 16, 2020",
+                            style:
+                                TextStyle(color: Colors.white38, fontSize: 12),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
               ),
-            ),
-            Container(
-              //color: Colors.blue,
-              width: MediaQuery.of(context).size.width,
-              height: 240,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                shrinkWrap: true,
-                itemCount: 3,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.asset("assets/popular_image.png"),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 6,bottom: 3),
-                          child: Text("Wonder Women (1984)",style: TextStyle(color: Color(0xffFFFFFF),fontSize: 14,fontWeight: FontWeight.w500),),
-                        ),
-                        Text("Des 16, 2020",style: TextStyle(color: Colors.white38),)
-                      ],
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: const Text(
+                    "Continue Watching",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xffFFFFFF),
                     ),
-                  );
-                },
+                  ),
+                ),
               ),
-            )
-          ],
+              Container(
+                //color: Colors.blue,
+                width: MediaQuery.of(context).size.width,
+                height: 220,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset("assets/watching.png"),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 6, bottom: 3),
+                            child: Text(
+                              "30 Monedas",
+                              style: TextStyle(
+                                  color: Color(0xffFFFFFF),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          const Text(
+                            "Des 16, 2020",
+                            style:
+                                TextStyle(color: Colors.white38, fontSize: 12),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
